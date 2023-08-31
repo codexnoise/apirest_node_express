3ae7becbd22c625e5cebf198265148b694c9bae0
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-08-30 20:59:20 -0500

    add: routes users and categories

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/new-route', (req, res) => {
   res.send('Hello from new route');
 });
 
-app.get('/my-products', (req, res) => {
+app.get('/products', (req, res) => {
   res.json([
     {
     name: 'Product 1',
@@ -22,8 +22,37 @@ app.get('/my-products', (req, res) => {
       name: 'Product 2',
       price: 55.55,
     }
-]);
+  ]);
 });
+
+app.get('/users', (req, res) =>{
+   const {limit, offset} = req.query;
+   if(limit && offset){
+    res.json({limit, offset})
+   }
+   res.send("Dont have params")
+})
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
+  res.json(
+    {
+      id: id,
+      name: 'Product 2',
+      price: 55.55,
+    }
+  );
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json(
+    {
+      categoryId,
+      productId,
+    }
+  );
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
