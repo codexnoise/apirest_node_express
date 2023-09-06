89f1a1f20b8dcfc07ea3899480837a00aa2475ac
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-09-05 21:55:20 -0500

    add: cors configuration to the project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routesApi = require('./routes')
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
@@ -7,8 +8,25 @@ const app = express();
 const port = 3006;
 
 app.use(express.json());
-
 app.use(express.static('public'));
+
+const whitelist = ['http://localhost:8080', 'http://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true)
+    }else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+
+app.use(cors(options));
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+routesApi(app);
 
 app.get('/', (req, res) => {
   res.send('Hello Fucking World !!!');
@@ -18,11 +36,6 @@ app.get('/new-route', (req, res) => {
   res.send('Hello from new route');
 });
 
-routesApi(app);
-
-app.use(logErrors);
-app.use(boomErrorHandler);
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
